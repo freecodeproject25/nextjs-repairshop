21c391034cf5a3605adb39392a01898c8cd9ef13
loc <lluu3790@gmail.com>
2025-05-11 12:51:29 -0500

    and sentry

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ npx shadcn@latest add button
     npm install next-themes
     npx shadcn@latest add dropdown-menu
     npm i @radix-ui/react-icons
+
+# sentry
+    npx @sentry/wizard@latest -i nextjs --saas --org llc-hw --project nextjs-repairshop
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://29151b26b1ac75513b8d201fe2346348@o4509302528212992.ingest.us.sentry.io/4509305704808448",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
