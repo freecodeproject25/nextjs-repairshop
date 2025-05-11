e8b0962f144505ca5f66de6adbb37dbc441f13f7
loc <lluu3790@gmail.com>
2025-05-11 12:28:12 -0500

    dark mode

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 npx shadcn@latest init
 npx shadcn@latest add button
+
+    npm install next-themes
+    npx shadcn@latest add dropdown-menu
+    npm i @radix-ui/react-icons
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,32 @@ const config = {
 	theme: {
 	  extend: {
 		backgroundImage: {
-		  'home-img': "url('/images/home-img.jpg')",
+			'home-img': "url('/images/home-img.jpg')",
+		},
+		keyframes: {
+			"appear": {
+				from: {
+					opacity: "0",
+				},
+				to: {
+					opacity: "1",
+				}
+			},
+			"slide": {
+				from: {
+					"transform": "translateX(100%)",
+				},
+				to: {
+					"transform": "translateX(0%)",
+				}
+			}
+		},
+		animation: {
+			"appear": "appear 1s ease-in-out",
+			"slide": "slide 750ms ease-in-out",
 		},
 	  },
+	  
 	},
 	plugins: ["@tailwindcss/postcss"],
   };
